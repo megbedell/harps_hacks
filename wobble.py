8124d79b11d57da7941e395d449414c78ddd7c37
megbedell <megbedell@gmail.com>
2016-05-20 14:43:17 -0500

    a few speed tweaks, thanks Julian

--- wobble.py
+++ wobble.py
@@ -163,7 +163,7 @@ def rv_parabola_fit(velocity, ccf):
     the RV minimum of each order's ccf (km/s)
     '''
     order_rvs = np.zeros(ccf.shape[0])
-    for i in np.arange(ccf.shape[0]):
+    for i in xrange(ccf.shape[0]):
         if i in [71, 66, 57]:  # disregard the bad orders
             order_rvs[i] = np.nan
             continue
@@ -182,7 +182,8 @@ def rv_gaussian_fit(velocity, ccf, n_points=80, debug=False):
     velocity (in km/s)
     ccf : np.ndarray
     ccf value
-    n_points : total number of points on either side of the minimum to fit
+    n_points : int
+    total number of points on either side of the minimum to fit, must be >= 2
 
     Returns
     -------
@@ -193,29 +194,29 @@ def rv_gaussian_fit(velocity, ccf, n_points=80, debug=False):
         print "Cannot fit a Gaussian to < 4 points! Try n_points = 2"
         return None
     order_rvs = np.zeros(ccf.shape[0])
-    for i in np.arange(ccf.shape[0]):
+    for i,order in enumerate(ccf):
         if i in [71, 66, 57]:  # disregard the bad orders
             order_rvs[i] = np.nan
             continue
-        ind_min = np.argmin(ccf[i])
+        ind_min = np.argmin(order)
         ind_range = np.arange(n_points*2+1) + ind_min - n_points
         if (ind_range > 160).any() or (ind_range < 0).any():
             print "n_points too large, defaulting to all"
             ind_range = np.arange(161)
-        height = max(ccf[i]) - min(ccf[i])
-        p0 = [-height, velocity[i,ind_min], 3.0, max(ccf[i])]
+        height = max(order) - min(order)
+        p0 = [-height, velocity[i,ind_min], 3.0, max(order)]
         if debug:
             print "order index {0}".format(i)
             print "starting param",p0
-        popt, pcov = curve_fit(gauss_function, velocity[i,ind_range], ccf[i,ind_range], p0=p0, maxfev=10000)
+        popt, pcov = curve_fit(gauss_function, velocity[i,ind_range], order[ind_range], p0=p0, maxfev=10000)
         order_rvs[i] = popt[1]
         if debug:
             print "solution param",popt
-            plt.scatter(velocity[i],ccf[i],color='blue')
-            plt.scatter(velocity[i,ind_range],ccf[i,ind_range],color='red')
+            plt.scatter(velocity[i],order,color='blue')
+            plt.scatter(velocity[i,ind_range],order[ind_range],color='red')
             x = np.arange(10000)/100.0 + velocity[i,0]
             plt.plot(x, gauss_function(x,popt[0],popt[1],popt[2],popt[3]))
-            plt.ylim(min(ccf[i])-500,max(ccf[i]))
+            plt.ylim(min(ccf[i])-500,max(order))
             plt.show()
     return order_rvs
 
@@ -240,9 +241,9 @@ if __name__ == "__main__":
     n_points = np.arange(2,80)
     order_rvs_gaussian = np.zeros((len(n_points),len(s.files), 73))
     pipeline_rv = np.zeros(len(s.files))
-    for i in np.arange(len(s.files)):
-        velocity, ccf, pipeline_rv[i] = read_ccfs(s.files[i])
-        for j in np.arange(len(n_points)):
+    for i,f in enumerate(s.files):
+        velocity, ccf, pipeline_rv[i] = read_ccfs(f)
+        for j in xrange(len(n_points)):
             order_rvs = rv_gaussian_fit(velocity, ccf, n_points=n_points[j])
             order_rvs_gaussian[j,i,:] = order_rvs
         print "file {0} of {1} finished".format(i+1,len(s.files))
@@ -266,6 +267,8 @@ if __name__ == "__main__":
     plt.xlim(0,80)
     plt.savefig('gaussian_coadded.png')
     plt.clf()
+    
+    pdb.set_trace()
     
         
     
