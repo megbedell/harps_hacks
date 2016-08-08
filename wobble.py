e14928063f96f5296dc429b1525cf3e3bc740ccc
megbedell <megbedell@gmail.com>
2016-08-08 13:54:44 -0500

    moved single-line fit to a function

--- wobble.py
+++ wobble.py
@@ -26,6 +26,36 @@ def plot_timeseries(time,rv,rv_err,rv2=0):
     plt.xlim(t.datetime[0]-datetime.timedelta(days=100), t.datetime[-1]+datetime.timedelta(days=100))
     plt.show()   
 
+def rv_oneline(wave_c,filenames):
+    '''Measure RV timeseries from a single line.
+    Parameters
+    ----------
+    wave_c : float
+    starting guess of the line's central wavelength
+    filenames : string (list)
+    list of CCF files
+    
+    Returns
+    -------
+    rv : np.float64 (list)
+    RV shift of the line at each epoch
+    '''
+    rv_one = np.zeros(len(filenames))
+    for i,f in enumerate(filenames):
+        # read in the spectrum
+        spec_file = str.replace(f, 'ccf_G2', 's1d')
+        sp = fits.open(spec_file)
+        header = sp[0].header
+        n_wave = header['NAXIS1']
+        crval1 = header['CRVAL1']
+        cdelt1 = header['CDELT1']
+        index = np.arange(n_wave)
+        wave = crval1 + index*cdelt1
+        spec = sp[0].data
+        # fit the line
+        rv_one[i] = read_harps.rv_oneline_fit(wave,spec,wave_c)
+    return rv_one
+
 if __name__ == "__main__":
     
     data_dir = "/Users/mbedell/Documents/Research/HARPSTwins/Results/"
@@ -57,24 +87,32 @@ if __name__ == "__main__":
     rv_aweightmed -= HIP54287.drift*1e-3 # apply the drift correction
     print "abs(a) weighted median RV RMS = {0:.3f} m/s".format(np.std(rv_aweightmed)*1.0e3)
     
+    #try refitting RVs with central CCF points excluded
+    rv_all = np.copy(HIP54287.data[:,:,1])
+    HIP54287.get_data(s.files, n_points=20, mask_inner=4)
+    rv_w = np.copy(HIP54287.data[:,:,1])  # RVs from CCF wings
+    HIP54287.get_data(s.files, n_points=4, mask_inner=0)
+    rv_c = np.copy(HIP54287.data[:,:,1])  # RVs from CCF centers
+    rv_diff = (rv_w - rv_c)/2.0
+    rv_avg = (rv_w + rv_c)/2.0
+    colors = iter(cm.gist_rainbow(np.linspace(0, 1, 69)))    
+    for i in range(69): plt.scatter(rv_avg[:,i], rv_diff[:,i], c=next(colors))
+    plt.ylabel(r'$(RV_c - RV_w)$/2')
+    plt.xlabel(r'$(RV_c + RV_w)$/2')
+    sm = plt.cm.ScalarMappable(cmap=cm.rainbow, norm=plt.Normalize(vmin=0, vmax=69))
+    sm._A = []
+    plt.colorbar(sm)
+    #plt.xlim([-0.5,9.5])
+    plt.savefig('fig/CCF_wingsvscenter.png')
+    plt.clf()
+    
+
     #fitting an individual line:
     wave_c = 6123.34
     #wave_c = 6043.2
     #wave_c = 6379.42
-    rv_one = np.zeros(len(s.files))
-    for i,f in enumerate(s.files):
-        # read in the spectrum
-        spec_file = str.replace(f, 'ccf_G2', 's1d')
-        sp = fits.open(spec_file)
-        header = sp[0].header
-        n_wave = header['NAXIS1']
-        crval1 = header['CRVAL1']
-        cdelt1 = header['CDELT1']
-        index = np.arange(n_wave)
-        wave = crval1 + index*cdelt1
-        spec = sp[0].data
-        # fit the line
-        rv_one[i] = read_harps.rv_oneline_fit(wave,spec,wave_c)
-    print "RMS on single-line RV (wavlength {0:.1f} A): {1:.2f} m/s".format(wave_c,np.std(rv_one)*1.0e3)
-    
+    rv_one = rv_oneline(wave_c, s.files)
+
+    print "RMS on single-line RV (wavelength {0:.1f} A): {1:.2f} m/s".format(wave_c,np.std(rv_one)*1.0e3)
+
         
